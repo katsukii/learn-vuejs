50bab7ffe326484255876074605ee9e7f4dc9269
Yusuke Katsuki <katsuki.yusuke@gmail.com>
2018-10-08 23:26:38 +0900

    v-onディレクティブ

---
+++ lecture2/v-on/script.js
@@ -0,0 +1,13 @@
+var app = new Vue({
+	el: '#app',
+	data: {
+		now: ''
+	}, 
+	methods: {
+		onclick: function() {
+			// alert('onclick!');
+			this.now = new Date().toLocaleString();
+		}
+	}
+
+})
