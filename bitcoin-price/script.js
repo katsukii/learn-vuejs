fb053817b04a2be9039e4c827e01981bb3d65dd2
Yusuke Katsuki <katsuki.yusuke@gmail.com>
2018-10-14 12:09:11 +0900

    bitcoin価格の表示

--- bitcoin-price/script.js
+++ bitcoin-price/script.js
@@ -2,6 +2,17 @@ var app = new Vue({
 	el: '#app',
 	data: {
 		bpi: null
+	}, 
+	mounted: function() {
+		axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+		.then(function(response){
+			console.log(response.data.bpi)
+			this.bpi = response.data.bpi
+			console.log(this.bpi)
+		}.bind(this))
+		.catch(function(error){
+			console.log(error)
+		})
 	}
 
 })
