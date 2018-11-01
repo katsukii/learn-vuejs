43b532aae218f8e0fe43b6569b064e91525f71f7
Yusuke Katsuki <katsuki.yusuke@gmail.com>
2018-11-01 11:21:31 +0900

    コンポーネントのオプション

---
+++ components/options/script.js
@@ -0,0 +1,9 @@
+Vue.component('button-counter', {
+  data: function() {
+    return { count: 0 }
+  }, 
+  template: '<div><span>count: </span><button v-on:click="count++">{{ count }}</button></div>'
+})
+var app = new Vue({
+  el: '#app',
+})
